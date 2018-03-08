8df9ae85734b6c3efad9cf1e7d0610ac5d148f7c
Dutchfreak <florida2525@hotmail.com>
2018-03-08 17:02:31 +0100

    Fixed yo shit bruh

--- MainForm.cs
+++ MainForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,8 +13,7 @@ namespace TestingGrounds
     public partial class MainForm : Form
     {
 
-        private BindingList<string> ItemsList;
-
+        private BindingList<Item> ItemsList; // chanced from string to item
         public MainForm()
         {
             InitializeComponent();
@@ -22,7 +21,7 @@ namespace TestingGrounds
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            ItemsList = new BindingList<string>();
+            ItemsList = new BindingList<Item>(); // chanced from string to item
 
             //ItemsList.Add("sword");
             //ItemsList.Add("bow");
@@ -39,29 +38,41 @@ namespace TestingGrounds
             sword1.Name = "Sword 1";
             Weapon sword2 = new Weapon();
             sword2.Name = "Sword 2";
-            ItemsList.Add(sword1.Name);
-            ItemsList.Add(sword2.Name);
+            ItemsList.Add(sword1);          // insert object instead of string
+            ItemsList.Add(sword2);          // insert object instead of string
             lstb_1.DataSource = ItemsList;
+            lstb_1.DisplayMember = "Name"; // This is important. we already said take the list of items to display
+                                           // with this line we say, only display the Name property of the item, dont display something ugly 
 
         }
 
         private void btn_add_Click(object sender, EventArgs e)
         {
-            ItemsList.Add(txtb_1.Text);
-            lstb_1.DataSource = ItemsList;
-            txtb_1.Clear();
+            //ItemsList.Add(txtb_1.Text);
+            //lstb_1.DataSource = ItemsList;
+            //txtb_1.Clear();
         }
 
         private void lstb_1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var selectedObject = lstb_1.SelectedItem;
-            string propertyCalled = "Name";
-            var propertyName = selectedObject.GetType().GetProperty(propertyCalled).GetValue(selectedObject, null);
 
-            //string selected = lstb_1.SelectedItem.ToString();
-            txtb_name.Text = propertyName.ToString();
-            //txtb_value.Text = selected.Value;
-            //txtb_weight.Text = selected.Weight;
+            Item SelectedObject = (Item)lstb_1.SelectedItem;
+            // we CAST the selected object to a Item object.
+            // we can do this because the Weapon types are deprived from the Item type
+            // this is not super save. if a object taht isnt deprived of Item is selected it goes boom(there are easy ways to check)
+
+            txtb_name.Text = SelectedObject.Name;
+            txtb_value.Text = SelectedObject.Value.ToString();
+            txtb_weight.Text = SelectedObject.Weight.ToString();
+
+            //Item selectedObject = lstb_1.SelectedItem;
+            //string propertyCalled = "Name";
+            //var propertyName = selectedObject.GetType().GetProperty(propertyCalled).GetValue(selectedObject, null);
+
+            ////string selected = lstb_1.SelectedItem.ToString();
+            //txtb_name.Text = propertyName.ToString();
+            ////txtb_value.Text = selected.Value;
+            ////txtb_weight.Text = selected.Weight;
         }
     }
 }
